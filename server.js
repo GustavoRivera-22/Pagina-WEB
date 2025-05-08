8732ede29fb7d338ff5c0a7240a743f571ead5f8
GustavoRivera-22 <riveragalazg@gmail.com>
2025-05-07 20:20:21 -0400

    Configuracion

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-Hola Mundo
+//Conexion del Server
+const mysql = required('mysql2')
+
+const Conexion = mysql.createconnection({
+    host: '10.0.6.39',
+    user: 'estudiante',
+    password: 'Informatica-165',
+    database: 'SEBAWEB'
+})
+
+Conexion.connect((err) => {
+    if (err) {
+        console.error('❌ Error al conectar a la base de datos:', err);
+      } else {
+        console.log('✅ Conectado a la base de datos MySQL.');
+      }
+})
+
